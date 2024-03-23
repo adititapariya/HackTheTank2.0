95afb511317a5ec75a5dfa940a9fa05c877a30a5
Vibhanshu230 <113599107+Vibhanshu230@users.noreply.github.com>
2024-03-24 00:06:42 +0530

    Home updated

--- sas_app/src/App.js
+++ sas_app/src/App.js
@@ -1,11 +1,31 @@
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Admin from "../src/pages/Admin/Admin";
+import { useLocation } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import ResponsiveAppBar from "./components/navbar";
 function App() {
+  const { pathname } = useLocation();
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0 });
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
   return (
     <div className="App">
       <div>
+        <nav className="fixed w-full z-50 top-0">
+          {pathname !== "/admin" && (
+            <>
+              <ResponsiveAppBar />
+            </>
+          )}
+        </nav>
         <Routes>
           <Route path="/admin" element={<Admin />}></Route>
           <Route path="/" element={<Home />}></Route>
--- sas_app/src/pages/Home/Home.jsx
+++ sas_app/src/pages/Home/Home.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import hero from "../../assets/hero.jpg";
+import "./Home.css";
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
 
 const Home = () => {
-  return <div>home</div>;
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    if (windowDimensions.width < 600) {
+      console.log("mobile view");
+      setIsMobile(true);
+    }
+  }, []);
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <div className="home">
+      <div>
+        <img src={hero} alt="jcomp" className="heroimg" />
+
+        <div className="homelanding">
+          <h1>SAS:Fat to Slim</h1>
+        </div>
+      </div>
+      <div className="section2">
+        <div className="aboutus">
+          <h2>Why Choose Us?</h2>
+        </div>
+        <div className="3waydivision">
+          <div className="divi1"></div>
+          <div className="divi2"></div>
+          <div className="divi3"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
