3ec4798b05b4f6ff5ffb060636c7a020c832c1eb
Vibhanshu230 <113599107+Vibhanshu230@users.noreply.github.com>
2024-03-23 21:37:30 +0530

    Updated apps

--- sas_app/src/pages/Admin/Admin.jsx
+++ sas_app/src/pages/Admin/Admin.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import { Routes, Route } from "react-router-dom";
 import Topbar from "./Topbar";
 import Sidebar from "./Sidebar";
@@ -8,25 +8,40 @@ import Team from "./Team";
 import Contacts from "./Contacts";
 import Invoices from "./Invoices";
 import Calendar from "./Calendar";
+import { useLocation } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "../../theme";
+// import "./Admin.css";
+import "../../index.css";
 
 const Admin = () => {
+  const { pathname } = useLocation();
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0 });
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <div classname="admin">
+        <div class="app">
           <Sidebar isSidebar={isSidebar} />
+
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
             <Dashboard />
-            <Team />
-            <Contacts />
-            <Invoices />
-            <Calendar />
+            {pathname === "/admin/team" && <Team />}
+            {pathname === "/admin/contact" && <Contacts />}
+            {pathname === "/admin/invoices" && <Invoices />}
+            {pathname === "/admin/calendar" && <Calendar />}
           </main>
         </div>
       </ThemeProvider>
