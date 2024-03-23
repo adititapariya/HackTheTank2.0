8d101bc9cd9cf5d9277754bf099e0f78a6b93a75
Vibhanshu230 <113599107+Vibhanshu230@users.noreply.github.com>
2024-03-23 16:32:10 +0530

    changes

--- htt-app/src/pages/global/Sidebar.jsx
+++ htt-app/src/pages/global/Sidebar.jsx
@@ -96,7 +96,7 @@ const Sidebar = () => {
                   alt="profile-user"
                   width="100px"
                   height="100px"
-                  src={`../../assets/user.png`}
+                  src={`../../assets/user.jpg`}
                   style={{ cursor: "pointer", borderRadius: "50%" }}
                 />
               </Box>
@@ -107,10 +107,10 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Ed Roh
+                  Vibhanshu
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
-                  VP Fancy Admin
+                  Admin
                 </Typography>
               </Box>
             </Box>
