8bff526d43e7b799d75e259f7cacb64f66811192
Vibhanshu230 <113599107+Vibhanshu230@users.noreply.github.com>
2024-03-23 15:32:11 +0530

    Topbar made

--- htt-app/src/App.js
+++ htt-app/src/App.js
@@ -1,5 +1,22 @@
+import { ColorModeContext, useMode } from "./theme";
+import Topbar from "./pages/global/Topbar";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 function App() {
-  return <div className="app"></div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+        ;
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
--- htt-app/src/theme.js
+++ htt-app/src/theme.js
@@ -197,4 +197,12 @@ export const ColorModeContext = createContext({
 export const useMode = () => {
   const [mode, setMode] = useState("dark");
 };
-const colorMode = useMemo(() => ({}));
+const colorMode = useMemo(
+  () => ({
+    toggleColorMode: () =>
+      setMode((prev) => (prev === "light" ? "dark" : "light")),
+  }),
+  []
+);
+const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+return [theme, colorMode];
