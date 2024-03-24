d501f09fc7f0dedd92b96368e892da8f32420703
Gourav Gupta <146537736+gouravg11@users.noreply.github.com>
2024-03-24 11:02:12 +0530

    Update WhatsApp.js

--- sas_app/src/WhatsApp.js
+++ sas_app/src/WhatsApp.js
@@ -1,8 +1,8 @@
 const cron = require('node-cron');
 const twilio = require('twilio');
 
-const accountSid = "ACab8104a8b687b76bda332f714325839f";
-const authToken = "3fba0f2d88ce756f5c99f0e194919a99";
+const accountSid = "xxxxxx";
+const authToken = "xxxxxxx";
 const client = twilio(accountSid, authToken);
 
 // Schedule the message to be sent every day at 9:44 : 35 AM
@@ -12,7 +12,7 @@ cron.schedule('35 44 09 * * *', () => {
     .create({
       from: 'whatsapp:+14155238886',
       body: 'Hello, there! This is your daily reminder.',
-      to: 'whatsapp:+917974138035'
+      to: 'whatsapp:number_to_send'
     })
     .then(message => console.log(message.sid))
     .catch(err => console.error('Error sending message:', err));
